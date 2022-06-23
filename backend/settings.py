9d2ccd7f73e2dbc60b7b1f25e96c40818fda2015
MavyCasta <106571381+MavyCasta@users.noreply.github.com>
2022-06-23 23:36:51 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bapa6-!b%clj$ob)l#@(_gnx+kw#m1u&-rei=dljx881ealtj)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['MaverickCasta.pythonanywhere.com']
 
 
 # Application definition
