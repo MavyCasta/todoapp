9c75c84f70c08a3d20a9b011f66131c16b0e5715
MavyCasta <106571381+MavyCasta@users.noreply.github.com>
2022-06-23 23:11:22 +0800

    Update todos.js

--- src/services/todos.js
+++ src/services/todos.js
@@ -4,36 +4,36 @@ class TodoDataService{
 
     getAll(token){
         axios.defaults.headers.common["Authorization"] = "Token " + token; 
-        return axios.get('https://MavyCasta.pythonanywhere.com/api/todos/');
+        return axios.get('https://MaverickCasta.pythonanywhere.com/api/todos/');
     }
     
 
     createTodo(data, token){ 
         axios.defaults.headers.common["Authorization"] = "Token " + token; 
-        return axios.get('https://MavyCasta.pythonanywhere.com/api/todos/');
+        return axios.get('https://MaverickCasta.pythonanywhere.com/api/todos/');
     }
 
     updateTodo(id, data, token){ 
         axios.defaults.headers.common["Authorization"] = "Token " + token; 
-        return axios.get('https://MavyCasta.pythonanywhere.com/api/todos/');
+        return axios.get('https://MaverickCasta.pythonanywhere.com/api/todos/');
     }
 
     deleteTodo(id, token){ 
         axios.defaults.headers.common["Authorization"] = "Token " + token; 
-        return axios.get('https://MavyCasta.pythonanywhere.com/api/todos/');
+        return axios.get('https://MaverickCasta.pythonanywhere.com/api/todos/');
     }
 
     completeTodo(id, token){ 
         axios.defaults.headers.common["Authorization"] = "Token " + token; 
-        return axios.get('https://MavyCasta.pythonanywhere.com/api/todos/');
+        return axios.get('https://MaverickCasta.pythonanywhere.com/api/todos/');
     }
 
     login(data){
-        return axios.get('https://MavyCasta.pythonanywhere.com/api/todos/');
+        return axios.get('https://MaverickCasta.pythonanywhere.com/api/todos/');
     }
 
     signup(data){
-        return axios.get('https://MavyCasta.pythonanywhere.com/api/todos/');
+        return axios.get('https://MaverickCasta.pythonanywhere.com/api/todos/');
     }
 }
 
